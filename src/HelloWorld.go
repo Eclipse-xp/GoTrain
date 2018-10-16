b544c6f8b4bd60dbf79263dbccb34c874124c892
xiexiangpeng <xie_xiangpeng@wuxiapptec.com>
2018-10-16 13:24:19 +0800

    archt adjust,add comments

--- src/HelloWorld.go
+++ src/HelloWorld.go
@@ -1,17 +1,15 @@
 package main
-
+//需要把项目目录加到GOPATH中，才能引用到项目中的自定义包。GOPATH可以包括多个目录
 import (
 	"strconv"
-	"fmt"
+	//点 . 点操作的含义就是这个包导入之后在调用这个包的函数时，可以省略前缀的包名，
+	// 也就是前面你调用的Println(“hello world”)  可以省略的写成Println(“hello world”)
+	."fmt"
 	"net/http"
 	"time"
 	"log"
-	"food"
-	"errors"
-	"os"
-	"bufio"
-	"strings"
-	//下划线表示 只执行保重init函数 并不引入全部文件,mysql/driver中有如下初始化操作，
+	"human"
+	//下划线表示 只执行包中init函数 并不引入全部文件,mysql/driver中有如下初始化操作，
 	//func init() {
 	//	sql.Register("mysql", &MySQLDriver{})//注册驱动
 	//}
@@ -21,22 +19,22 @@ import (
 	"github.com/jinzhu/gorm"
 )
 
-var xp Person
-var persons []Person
-var theCouple map[string]Person
+var xp human.Person
+var persons []human.Person
+var theCouple map[string]human.Person
 var language = "java"
 
 func main() {
-	//init_local()
+	init_local()
 	//
-	//introduceAll(persons)
+	introduceAll(persons)
 	//introduceCP(theCouple)
 	//testUpdateVar()
 	//
 	//xp.autoIntroduce()
 	//
 	//xp.travel("北京")
-	//fmt.Println(getIntroduce(xp))
+	//Println(getIntroduce(xp))
 	//
 	//var man human.Ability = &xp
 	//man.Learn("java")
@@ -51,11 +49,11 @@ func main() {
 	//wife := theCouple["wife"]
 	//err := wife.Eat(&xiaolongxia, "辣")
 	//if err!=nil {
-	//	fmt.Println("拒绝吃饭！", err)
+	//	Println("拒绝吃饭！", err)
 	//}
 	//
 	//if err := recover();err != nil {
-	//	fmt.Println(err)
+	//	Println(err)
 	//}
 	//xp.fallInLoveWith(&wife)
 	//
@@ -64,17 +62,17 @@ func main() {
 	//c := make(chan int, 2)
 	//c <- 1
 	//c <- 2
-	//fmt.Println(<-c)
-	//fmt.Println(<-c)
+	//Println(<-c)
+	//Println(<-c)
 
-	operateGORM()
+	//operateGORM()
 	//operateDB()
 }
 
 //____________本应放在其他文件的内容，由于编译问题暂放这里________________
-
+type hperson human.Person
 //开放博客
-func (p *Person) showBlog() {
+func (p *hperson) showBlog() {
 	mux := http.NewServeMux()
 	rh := http.RedirectHandler("http://744722813.iteye.com/", 307)
 	mux.Handle("/blog", rh)
@@ -94,94 +92,17 @@ func timeHandler(format string) http.Handler {
 	//下面是接收json并反序列化的方式
 	//body, err := ioutil.ReadAll(r.Body)
 	//if err != nil {
-	//	fmt.Printf("read body err, %v\n", err)
+	//	Printf("read body err, %v\n", err)
 	//	return
 	//}
 	//println("json:", string(body))
 	//
 	//var a AutotaskRequest
 	//if err = json.Unmarshal(body, &a); err != nil {
-	//	fmt.Printf("Unmarshal err, %v\n", err)
+	//	Printf("Unmarshal err, %v\n", err)
 	//	return
 	//}
-	//fmt.Printf("%+v", a)
-}
-
-type Person struct {
-	//姓名
-	name string
-	//性别
-	sex string
-	//年龄
-	age int
-	//所在地
-	local string
-	//技能
-	skill []string
-	//体重
-	weight int
-}
-
-//学习
-func (p *Person) Learn(learnContent string) {
-	p.skill = append(p.skill, learnContent)
-}
-
-//吃饭
-func (p *Person) Eat(food *food.Food, avoid string) error {
-	//处理 味道忌口
-	for _, taste := range food.Taste {
-		if strings.Contains(taste, avoid) {
-			return errors.New("忌口：" + avoid)
-		}
-	}
-	p.weight += 1
-	return nil
-}
-
-//自我介绍
-func (p *Person) autoIntroduce() {
-	fmt.Println("Hey guys, my name is " + p.name)
-	fmt.Println("I do well in ", p.skill)
-}
-
-//旅行
-func (p *Person) travel(city string) {
-	p.local = city
-}
-
-//读书
-func (p *Person) readBook() {
-	path := "E:\\test\\背影.txt"
-	log.Print(path)
-	file, err := os.Open(path)
-
-	if err == nil {
-		scanner := bufio.NewScanner(file)
-		for scanner.Scan() {
-			fmt.Println(scanner.Text())
-		}
-	} else {
-		fmt.Println("file error:", err)
-	}
-
-}
-
-//做饭
-func (p *Person) cook() food.Food {
-
-	return food.Food{
-		Name:  "小龙虾",
-		Taste: []string{"麻辣"},
-	}
-}
-
-//恋爱
-func (p *Person) fallInLoveWith(who *Person) {
-	if p.sex == who.sex {
-		panic("对不起，我" + p.name + "是直的！", )
-	}
-	fmt.Println(p.name, " love ", who.name)
+	//Printf("%+v", a)
 }
 
 //____________________________
@@ -189,53 +110,53 @@ func (p *Person) fallInLoveWith(who *Person) {
 func testUpdateVar() {
 	//直接赋值
 	person2 := xp
-	person2.local = "吉林"
-	fmt.Println(getIntroduce(xp))
+	person2.Local = "吉林"
+	Println(getIntroduce(xp))
 	//通过指针
 	language3 := &language
 	*language3 = "go"
 	person3 := &xp
-	person3.local = "吉林"
-	fmt.Println(getIntroduce(xp))
+	person3.Local = "吉林"
+	Println(getIntroduce(xp))
 
 }
 
 //初始化变量
 func init_local() {
-	xp = Person{
-		name:  "xiexiangpeng",
-		sex:   "男",
-		age:   18,
-		local: "吉林啊",
+	xp = human.Person{
+		Name:  "xiexiangpeng",
+		Sex:   "男",
+		Age:   18,
+		Local: "吉林啊",
 	}
-	wy := Person{
-		name:  "wangwenya",
-		sex:   "女",
-		age:   14,
-		local: "廊坊",
+	wy := human.Person{
+		Name:  "wangwenya",
+		Sex:   "女",
+		Age:   14,
+		Local: "廊坊",
 	}
 
-	persons = []Person{xp, wy}
+	persons = []human.Person{xp, wy}
 	//theCouple = make(map[string]Person)
-	theCouple = map[string]Person{"husband": xp, "wife": wy}
+	theCouple = map[string]human.Person{"husband": xp, "wife": wy}
 
 }
-func introduceCP(cp map[string]Person) {
+func introduceCP(cp map[string]human.Person) {
 	for role, w := range cp {
-		fmt.Println(role + ":" + getIntroduce(w))
+		Println(role + ":" + getIntroduce(w))
 	}
 }
 
 //介绍全员
-func introduceAll(persons []Person) {
+func introduceAll(persons []human.Person) {
 	for _, man := range persons {
-		fmt.Println("this is " + man.name + "," + strconv.Itoa(man.age) + " years old. come from " + man.local)
+		Println("this is " + man.Name + "," + strconv.Itoa(man.Age) + " years old. come from " + man.Local)
 	}
 }
 
 //获得一个人的简介
-func getIntroduce(person Person) string {
-	return "this is " + person.name + "," + strconv.Itoa(person.age) + " years old. come from " + person.local
+func getIntroduce(person human.Person) string {
+	return "this is " + person.Name + "," + strconv.Itoa(person.Age) + " years old. come from " + person.Local
 }
 
 type man struct {
@@ -249,7 +170,7 @@ func operateDB() {
 	//db 类型为sql.DB
 	db, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/mytest?charset=utf8")
 	if err != nil {
-		fmt.Println(err)
+		Println(err)
 	}
 	//preStmt, _ := db.Prepare("insert into test_tab(name,city) values (?,?)")
 	//preStmt.Exec("tr", "beijing")
@@ -261,23 +182,23 @@ func operateDB() {
 	stmt.Exec("yf", "衡水")
 	transaction.Commit()
 
-	fmt.Println("--------批量查询--------")
+	Println("--------批量查询--------")
 	rows, _ := db.Query("select * from test_tab")
 	for rows.Next() {
 		var id int
 		var name, city string
 		if err := rows.Scan(&id, &name, &city); err == nil {
-			fmt.Println("id:", id, " name:", name, " city:", city)
+			Println("id:", id, " name:", name, " city:", city)
 		}
 	}
 
-	fmt.Println("--------单个查询--------")
+	Println("--------单个查询--------")
 	var id int
 	var name, city string
 	db.QueryRow("select * from test_tab where id = 4").Scan(&id, &name, &city)
 	//var record man
 	//db.QueryRow("select * from test_tab where id = 1").Scan(&record)
-	//fmt.Println(record)
+	//Println(record)
 	db.Close()
 
 }
---
+++ src/human/Person.go
@@ -0,0 +1,89 @@
+//go的风格不建议过多建立文件夹，所以有依赖的结构体尽量在一个包中
+package human
+
+import (
+	"log"
+	"os"
+	"bufio"
+	"fmt"
+	"food"
+	"strings"
+	"errors"
+)
+
+type Person struct {
+	//姓名
+	Name string
+	//性别
+	Sex string
+	//年龄
+	Age int
+	//所在地
+	Local string
+	//技能
+	Skill []string
+	//体重
+	Weight int
+}
+
+//学习
+func (p *Person) Learn(learnContent string) {
+	p.Skill = append(p.Skill, learnContent)
+}
+
+//吃饭
+func (p *Person) Eat(food *food.Food, avoid string) error {
+	//处理 味道忌口
+	for _, taste := range food.Taste {
+		if strings.Contains(taste, avoid) {
+			return errors.New("忌口：" + avoid)
+		}
+	}
+	p.Weight += 1
+	return nil
+}
+
+//自我介绍
+func (p *Person) AutoIntroduce() {
+	fmt.Println("Hey guys, my name is " + p.Name)
+	fmt.Println("I do well in ", p.Skill)
+}
+
+//旅行
+func (p *Person) Travel(city string) {
+	p.Local = city
+}
+
+//读书
+func (p *Person) ReadBook() {
+	path := "E:\\test\\背影.txt"
+	log.Print(path)
+	file, err := os.Open(path)
+
+	if err == nil {
+		scanner := bufio.NewScanner(file)
+		for scanner.Scan() {
+			fmt.Println(scanner.Text())
+		}
+	} else {
+		fmt.Println("file error:", err)
+	}
+
+}
+
+//做饭
+func (p *Person) Cook() food.Food {
+
+	return food.Food{
+		Name:  "小龙虾",
+		Taste: []string{"麻辣"},
+	}
+}
+
+//恋爱
+func (p *Person) FallInLoveWith(who *Person) {
+	if p.Sex == who.Sex {
+		panic("对不起，我" + p.Name + "是直的！", )
+	}
+	fmt.Println(p.Name, " love ", who.Name)
+}
